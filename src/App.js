e76a70d946c648a33b578c41bc4109d1f1362d8e
lucidck <curtiskilgore@hotmail.com>
2020-07-09 12:27:34 -0700

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import "./App.css";
 
 class App extends React.Component {
@@ -12,7 +13,7 @@ class App extends React.Component {
       headerLinks: [
         { title: "Home", path: "/" },
         { title: "About", path: "/about" },
-        { title: "Home", path: "/home" },
+        { title: "Contact", path: "/contact" },
       ],
       home: {
         title: "Full Stack Web Developer",
@@ -31,8 +32,22 @@ class App extends React.Component {
     return (
       <Router>
         <Container fluid={true} className="p-0">
-          <Navbar className="border-bottom">
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
             <Navbar.Brand>Curtis Kilgore</Navbar.Brand>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
           </Navbar>
         </Container>
       </Router>
