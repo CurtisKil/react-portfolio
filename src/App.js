6696b345ab2ec8c59d6f2ea394aa058165a3e09f
lucidck <curtiskilgore@hotmail.com>
2020-07-09 12:05:11 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Curtis Kilgore",
+      headerLinks: [
+        { title: "Home", path: "/" },
+        { title: "About", path: "/about" },
+        { title: "Home", path: "/home" },
+      ],
+      home: {
+        title: "Full Stack Web Developer",
+        subTitle: "Always Learning, Always Growing",
+        text: "Checkout My Projects",
+      },
+      about: {
+        title: "About Me",
+      },
+      contact: {
+        title: "Reach Out To Me",
+      },
+    };
+  }
+  render() {
+    return (
+      <Router>
+        <Container fluid={true} className="p-0">
+          <Navbar className="border-bottom">
+            <Navbar.Brand>Curtis Kilgore</Navbar.Brand>
+          </Navbar>
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
